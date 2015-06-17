c4898260dd86d77b6b102b2b73a8df2e94ba997d
Chang <changwoo92@gmail.com>
2015-06-17 15:58:48 -0700

    First commit

---
+++ MusicBar/MusicBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicBar
+//
+//  Created by Jake Choi on 6/17/15.
+//  Copyright (c) 2015 Sole Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
