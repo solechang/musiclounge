1ce98202fe4620c5a3b179245cd88a2513bfb28f
Chang <changwoo92@gmail.com>
2015-08-06 17:42:07 -0700

    Adding crashlytics

--- MusicBar/Pods/Target Support Files/Pods-MusicBar/Pods-MusicBar-environment.h
+++ MusicBar/Pods/Target Support Files/Pods-MusicBar/Pods-MusicBar-environment.h
@@ -72,6 +72,12 @@
 #define COCOAPODS_VERSION_MINOR_CocoaSoundCloudUI 0
 #define COCOAPODS_VERSION_PATCH_CocoaSoundCloudUI 9
 
+// Crashlytics
+#define COCOAPODS_POD_AVAILABLE_Crashlytics
+#define COCOAPODS_VERSION_MAJOR_Crashlytics 3
+#define COCOAPODS_VERSION_MINOR_Crashlytics 1
+#define COCOAPODS_VERSION_PATCH_Crashlytics 0
+
 // DZNEmptyDataSet
 #define COCOAPODS_POD_AVAILABLE_DZNEmptyDataSet
 #define COCOAPODS_VERSION_MAJOR_DZNEmptyDataSet 1
@@ -83,6 +89,18 @@
 #define COCOAPODS_VERSION_MAJOR_DZNSegmentedControl 1
 #define COCOAPODS_VERSION_MINOR_DZNSegmentedControl 2
 #define COCOAPODS_VERSION_PATCH_DZNSegmentedControl 2
+
+// Fabric
+#define COCOAPODS_POD_AVAILABLE_Fabric
+#define COCOAPODS_VERSION_MAJOR_Fabric 1
+#define COCOAPODS_VERSION_MINOR_Fabric 2
+#define COCOAPODS_VERSION_PATCH_Fabric 8
+
+// Fabric/Base
+#define COCOAPODS_POD_AVAILABLE_Fabric_Base
+#define COCOAPODS_VERSION_MAJOR_Fabric_Base 1
+#define COCOAPODS_VERSION_MINOR_Fabric_Base 2
+#define COCOAPODS_VERSION_PATCH_Fabric_Base 8
 
 // FreeStreamer
 #define COCOAPODS_POD_AVAILABLE_FreeStreamer
