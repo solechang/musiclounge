eafd6b985755fdcdca065b73cfbefa8c8d35d666
Chang <changwoo92@gmail.com>
2015-09-09 16:03:07 -0700

    Working on Feedback

---
+++ MusicBar/MusicBar/FeedbackViewController.m
@@ -0,0 +1,50 @@
+//
+//  FeedbackViewController.m
+//  MusicLounge
+//
+//  Created by Jake Choi on 9/9/15.
+//  Copyright (c) 2015 Sole Chang. All rights reserved.
+//
+
+#import "FeedbackViewController.h"
+
+@interface FeedbackViewController ()
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sendButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backButton;
+
+@end
+
+@implementation FeedbackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    
+    [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc] init] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setShadowImage:[[UIImage alloc] init]];
+    
+    [self.tabBarController.tabBar setTintColor:[UIColor whiteColor]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)backButtonPressed:(id)sender {
+}
+- (IBAction)sendButtonPressed:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
